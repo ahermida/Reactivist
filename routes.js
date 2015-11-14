7cc2b2fa9b263b919486e96833aa4c0a3c5e88b2
Kent <kentkwu@gmail.com>
2015-11-14 04:48:13 -0500

    Added user routes

--- routes.js
+++ routes.js
@@ -12,17 +12,39 @@ var express     = require('express');
 var router      = express.Router();
 var url         = config.mongouid;
 
-// var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, process.env.HOME + '/public/img');
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, file.fieldname + '-' + Date.now() + ".jpg");
-//     }
-// });
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, process.env.HOME + '/public/img');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + ".jpg");
+    }
+});
 
 /** Upload Multer Handler (Setup) */
-//var upload = multer({ storage: storage });
+var upload = multer({ storage: storage });
+
+router.get('/users/:id', function(req, res) {
+    MongoClient.connect(url, function(err, db) {
+        var collection = db.collection('posts');
+        collection.find({id:req.params.id}).toArray(function(err, result) {
+            res.json({answer: result});
+            db.close();
+        });
+    });
+});
+
+router.post('/users', function(req, res) {
+    var user = req.body;
+    console.log("Adding user: " + JSON.stringify(user));
+    MongoClient.connect(url, function(err, db) {
+        var collection = db.collection('users');
+        collection.insert(user, function(err, result) {
+            res.json({answer: result});
+            db.close();
+        });
+    });
+});
 
 router.get('/posts', function(req, res) {
 	MongoClient.connect(url, function(err, db) {
@@ -50,7 +72,7 @@ router.post('/posts', function(req, res) {
     console.log("Adding post: " + JSON.stringify(post));
     MongoClient.connect(url, function(err, db) {
         var collection = db.collection('posts');
-        collection.insert({"_id":5, "topic": "topic"}, function(err, result) {
+        collection.insert(post, function(err, result) {
             res.json({answer: result});
             db.close();
         });
