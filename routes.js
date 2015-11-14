6692fb95b5fd505941daf9ac76d92a7196d5e31b
Albert Hermida <alberthermida@gmail.com>
2015-11-14 01:55:26 -0500

    added server boilerplate

---
+++ routes.js
@@ -0,0 +1,24 @@
+/**
+ * API Routes for HackHarvard
+ */
+var fs                = require('fs');
+var config            = require('./config');
+var multer            = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, process.env.HOME + '/public');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + ".jpg");
+    }
+});
+
+/** Upload Multer Handler (Setup) */
+var upload = multer({ storage: storage });
+
+module.exports = function(express) {
+
+  /** Export router to give Express access to routes */
+  return router;
+};
---
+++ server.js
@@ -0,0 +1,40 @@
+/**
+ *  Food Classification App for Android Hackathon at VU
+ *  Authors: David Siah, Albert Hermida, Will Makabenta, Jo Warren
+ */
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var config      = require('./config');
+var morgan      = require('morgan');
+var routes      = require('./routes')(express);
+var fs          = require('fs');
+
+/** Set Headers */
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
+/** Set Public Directory to Static */
+app.use(express.static(__dirname + '/public'));
+
+/** Just What They Say */
+app.get('/', function(req, res) {
+  res.json({'code drunk': 'compile sober'});
+});
+
+/** Log Requests */
+app.use(morgan('dev'));
+
+/** Import Routes */
+app.use('/api', routes);
+
+
+/** Startup Server on config.port */
+app.listen(config.port);
+console.log('App running on http://localhost:' + config.port);
