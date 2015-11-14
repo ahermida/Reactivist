faad3ca55f65c3b988a6bc98969e2f43603e39d2
david siah <dsiah@villanova.edu>
2015-11-14 13:42:25 -0500

    removing crash-files

--- server.js
+++ server.js
@@ -5,7 +5,6 @@
 var express     = require('express');
 var app         = express();
 var bodyParser  = require('body-parser');
-var config      = require('./config');
 var morgan      = require('morgan');
 var routes      = require('./routes');
 var fs          = require('fs');
