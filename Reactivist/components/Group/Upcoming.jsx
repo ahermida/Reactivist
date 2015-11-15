b2876a9a27ffeaa36bf8cc30cbfd5ac3614e7b36
Albert Hermida <alberthermida@gmail.com>
2015-11-15 02:45:28 -0500

    added upcoming boilerplate

---
+++ Reactivist/components/Group/Upcoming.jsx
@@ -0,0 +1,19 @@
+/**
+ * Component for Upcoming Button
+ */
+var React             = require('react');
+
+/**
+ * Component
+ */
+module.exports = React.createClass({
+
+  render: function() {
+    //Reactivist view
+    return (
+    <div className="Upcoming">
+      
+    </div>);
+  }
+
+});
