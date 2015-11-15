59bf5560055cb883e87047aa327856e7fa772074
Albert Hermida <alberthermida@gmail.com>
2015-11-15 03:15:35 -0500

    fixed client.js

--- Reactivist/components/Group/Upcoming.jsx
+++ Reactivist/components/Group/Upcoming.jsx
@@ -1,18 +1,15 @@
 /**
  * Component for Upcoming Button
  */
-var React             = require('react');
+var React = require('react');
 
-/**
- * Component
- */
 module.exports = React.createClass({
 
   render: function() {
     //Reactivist view
     return (
     <div className="Upcoming">
-      
+
     </div>);
   }
 
