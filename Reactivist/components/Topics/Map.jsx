27f79d80ea8e49b020c080e783c170ac5e4b36d4
david siah <dsiah@villanova.edu>
2015-11-14 21:41:15 -0500

    2 goog 2 furious

--- Reactivist/components/Topics/Map.jsx
+++ Reactivist/components/Topics/Map.jsx
@@ -1,19 +1,19 @@
 var React = require('react');
 var Map = React.createClass({
-	componentDidMount: function() {
-		initMap();
-	},
+  componentDidMount: function() {
+    initMap();
+  },
 
-	render: function() {
-  		window.initMap = function() {
-			map = new google.maps.Map(document.getElementById('map'), {
-	    		center: {lat: -34.397, lng: 150.644},
-	    		zoom: 8
-	  		});
-		}
-  		
-  		return <div id="map" ></div>;
-	}
+  render: function() {
+    window.initMap = function() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+      });
+    }
+      
+    return <div id="map" ></div>;
+  }
 });
 
 module.exports = Map;
